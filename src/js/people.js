574754934f5f6f531ae7a7eb5e967d5db0a99fbd
dengxiaodi <dixiaodeng@gmail.com>
2016-08-07 21:05:23 +0800

    user homepage 70%

--- src/js/people.js
+++ src/js/people.js
@@ -34,3 +34,25 @@ $(document).ready(function(){
         }
      });
 });
+
+$(document).ready(function(){
+	function onListLoadComplete(element, complete) {
+		if(complete) {
+			element.addClass('no-more');
+            element.html('已加载全部内容');
+		}
+	}
+
+	if(typeof PEOPLE_USER_ID != 'undefined') {
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/topics/uid-' + PEOPLE_USER_ID, $('#flw-topics-load-more'), $('#flw-topic-list'), 2, onListLoadComplete);
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/follows/type-follows__uid-' + PEOPLE_USER_ID, $('#flw-friends-load-more'), $('#flw-friend-list'), 2, onListLoadComplete);
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/follows/type-fans__uid-' + PEOPLE_USER_ID, $('#flw-fans-load-more'), $('#flw-fans-list'), 2, onListLoadComplete);
+
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/questions/type-answered__uid-' + PEOPLE_USER_ID, $('#usrq-answered-load-more'), $('#usrq-answered-list'), 2, onListLoadComplete);
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/questions/type-failed__uid-' + PEOPLE_USER_ID, $('#usrq-failed-load-more'), $('#usrq-failed-list'), 2, onListLoadComplete);
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/questions/type-publish__uid-' + PEOPLE_USER_ID, $('#usrq-publish-load-more'), $('#usrq-publish-list'), 2, onListLoadComplete);
+		NKR.load_list_view(G_BASE_URL + '/people/ajax/answers/uid-' + PEOPLE_USER_ID, $('#usrq-answer-load-more'), $('#usrq-answer-list'), 2, onListLoadComplete);
+	}
+
+	NKR.load_list_view(G_BASE_URL + '/notifications/ajax/list/', $('#notifications-load-more'), $('#notification-list'), 1, onListLoadComplete);
+});
